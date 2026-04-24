e1325223e5b15ec614e88b8cf194d595fb180f45
michael seaman <mseaman26@gmail.com>
2026-04-24 02:15:37 -0700

    adjustment

--- app/page.js
+++ app/page.js
@@ -185,7 +185,10 @@ const LineGraph = () => {
         const ctx = chartRef?.current?.getContext("2d");
         //t ratio can be adjusted here by changing the final multiplier
         let aspecRatio = null
-        aspecRatio = (window?.innerWidth - 100)/(aspecheight * (window?.innerWidth - 100)) * 20
+        aspecRatio = (window?.innerWidth - 100)/(aspecheight * (window?.innerWidth - 100)) * 27
+        
+
+
 
         
         console.log('aspect ratio: ', aspecRatio)
