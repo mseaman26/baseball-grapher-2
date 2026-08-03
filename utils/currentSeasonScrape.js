05530bc09516dcddfca3bb8957b62c1330b83906
michael seaman <mseaman26@gmail.com>
2026-08-03 14:58:50 -0700

    all star break handling and scaling

--- utils/currentSeasonScrape.js
+++ utils/currentSeasonScrape.js
@@ -103,8 +103,8 @@ const scrapeCurrentSeason = async (teamName) => {
         
             } 
             //all star break. had to find the day (116) manually and add four days off (8 ticks)
-            if(currentDay === 116){
-                for(let i = 0; i < 8; i++){
+            if(currentDay === 110){
+                for(let i = 0; i < 6; i++){
                     currentDay += .5
                     labels.push(currentDay)
                     standings.push(currentStanding)
